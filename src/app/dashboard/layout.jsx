375adf52109d213452e52f471184731fdf3901bf
mdallamaiqbal <bd999plus13@gmail.com>
2026-06-22 22:00:06 +0600

    add where artist can post so thats why i make a form when artist filap form then upload

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -3,7 +3,7 @@ import { DashboardSidebar } from '../components/dashboard/DashboardSidebar';
 
 const DashboardLayout = ({children}) => {
     return (
-        <div className='flex  min-h-screen'>
+        <div className='sm:flex min-h-screen'>
             <DashboardSidebar />
             <div className='flex-1'>{children}</div>
         </div>
