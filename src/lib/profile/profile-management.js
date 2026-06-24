4d77ca681de79b3207aab5d19574d57eb08381ce
mdallamaiqbal <bd999plus13@gmail.com>
2026-06-24 22:09:32 +0600

    add profile management

---
+++ src/lib/profile/profile-management.js
@@ -0,0 +1,17 @@
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const getUserProfile = async (email) => {
+    const res = await fetch(`${baseUrl}/api/users?email=${email}`, {
+        cache: 'no-store'
+    });
+    return res.json();
+};
+
+export const updateUserProfile = async (profileData) => {
+    const res = await fetch(`${baseUrl}/api/users/update-profile`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(profileData)
+    });
+    return res.json();
+};
